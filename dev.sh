acd933fa5d59dd03bd1d4a4547e5f07b327805c3
xuchaojie <chaojie.xu@visionacademy.cn>
2024-10-28 15:42:27 +0800

    feat: 页面及配置初次提交

---
+++ dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# 加载 nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+nvm use
+vitepress dev docs
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+  base: '/tomato-vitepress/', // 替换为你的仓库名
+  // 其他配置...
+})
